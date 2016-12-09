c8577a61dd8abaafc21760264056854e6408a4ff
Abola Lee <abola921@gmail.com>
2016-12-09 13:54:26 +0800

    調整範例內容

---
+++ src/crawler/example/past/PttHomework.java
@@ -0,0 +1,28 @@
+package crawler.example.past;
+
+import com.github.abola.crawler.CrawlerPack;
+
+
+/**
+ * 簡易練習
+ * 
+ * 找出所有文章中按推的id
+ * 
+ * @author Abola Lee
+ *
+ */
+public class PttHomework {
+	
+	public static void main(String[] args) {
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1458547807.A.32A.html";
+		
+
+		System.out.println( 
+			CrawlerPack.start()
+			    .addCookie("over18", "1")
+				.getFromHtml(uri)
+				.select(".push-tag:matchesOwn(推) + .push-userid")
+				.toString()
+		);
+	}
+}
