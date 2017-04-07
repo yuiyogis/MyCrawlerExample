95b0430b365080e0f3446df03b13e371c5f1175b
Abola Lee <abola921@gmail.com>
2017-04-07 23:38:36 +0800

    移動範例

---
+++ src/crawler/example/past/Stock2330.java
@@ -0,0 +1,34 @@
+package crawler.example.past;
+
+import com.github.abola.crawler.CrawlerPack;
+
+/**
+ * 練習擷取證交所提供的盤後資訊
+ * 
+ * 重點
+ * 1. 如何使用 Regular Expression 取出表格中的數值資料
+ * 
+ * @author Abola Lee
+ *
+ */
+public class Stock2330 {
+	
+	public static void main(String[] args) {
+		// 2330 
+		String uri = "http://www.twse.com.tw"
+				+ "/ch/trading/exchange/STOCK_DAY/genpage"
+				+ "/Report201602/201602_F3_1_8_2330.php"
+				+ "?STK_NO=2330&myear=2016&mmon=03";
+		
+		System.out.println( 
+			CrawlerPack.start()
+				.setRemoteEncoding("big5")
+				.getFromHtml(uri)
+				.select("td:matchesOwn(^[\\+\\-]?([0-9]{1,3},)*[0-9]{1,3}(\\.[0-9]+)*$)")
+//				.select("script")
+//				.toString()
+		);
+		
+		
+	}
+}
